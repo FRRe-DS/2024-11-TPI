b3e1fd824b33d856b166966ba0c50bacbd8251aa
santiago <santiagov.scacciaferrow@gmail.com>
2024-11-12 18:37:51 -0300

    DREAM TEAM

--- packages/frontend/src/layout/Navbar/Navbar.tsx
+++ packages/frontend/src/layout/Navbar/Navbar.tsx
@@ -12,30 +12,40 @@ function Navbar() {
     };
 
     return (
-        <div>
+        <div className="relative">
+            {/* Video de fondo */}
+            <div className="absolute inset-0 overflow-hidden -z-10">
+                <video
+                    src="https://www.bienaldelchaco.org/2024/wp-content/uploads/2024/05/B24-web-slide-institucional-nuevo-c.mp4"
+                    autoPlay
+                    loop
+                    muted
+                    className="w-full h-full object-cover"
+                />
+            </div>
+
             {/* Navbar para pantallas grandes */}
-            <nav className="hidden md:flex justify-between items-center p-4 bg-gray-800 text-white shadow-lg">
-                <NavbarLogo/>
-                <NavbarLinks/>
-                <UserMenu/>
+            <nav className="hidden md:flex justify-between items-center p-4 bg-gray-800 bg-opacity-60 text-white shadow-lg relative">
+                <NavbarLogo />
+                <NavbarLinks />
+                <UserMenu />
             </nav>
 
             {/* Navbar para pantallas móviles */}
-            <nav className="md:hidden bg-gradient-to-r from-purple-600 via-blue-600 to-teal-600 shadow-md">
+            <nav className="md:hidden bg-gray-800 bg-opacity-60 shadow-md relative">
                 <div className="flex justify-between items-center p-4">
-                    <NavbarLogo/>
+                    <NavbarLogo />
                     <button onClick={toggleSidebar} className="text-white focus:outline-none hover:text-teal-200">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
                              className="h-8 w-8">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
-                                  d="M4 6h16M4 12h16m-7 6h7"/>
+                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
                         </svg>
                     </button>
                 </div>
             </nav>
 
             {/* Sidebar y Overlay para pantallas móviles */}
-            <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar}/>
+            <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
         </div>
     );
 }
