faa2f3c14f4ea08976c0183c5046d544801a52e1
santiago <santiagov.scacciaferrow@gmail.com>
2024-12-03 00:50:59 -0300

    cambios para la nube

--- packages/frontend/src/features/home/components/Maps.tsx
+++ packages/frontend/src/features/home/components/Maps.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Footer from "./ui/Footer.tsx";
+import logo from "../assets/images/B22-Slide-web-footer-v4.fw_.png";
 
 const Maps: React.FC = () => {
     return (
@@ -15,9 +15,54 @@ const Maps: React.FC = () => {
                 className="w-full h-[calc(102vh-70px)] sm:h-[calc(72vh-80px)]"  // Ajuste del alto según el tamaño de la pantalla, para móviles y PC
                 loading="lazy"  // Carga perezosa para mejorar el rendimiento
             ></iframe>
+            <footer
+                className="fixed bottom-0 left-0 w-full bg-cover bg-center text-white"
+                style={{
+                    backgroundImage: `url('https://plus.unsplash.com/premium_photo-1719864933065-6639a2d32e56?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`,
+                }}
+            >
+                <div
+                    className="flex flex-col-reverse sm:flex-row justify-between items-center px-4 py-4 sm:px-5 sm:py-8 md:px-6 md:py-24">
+                    {/* Redes sociales */}
+                    <div className="flex flex-col justify-center items-end gap-4">
+                        <div className="flex justify-end gap-6 mt-6">
+                            {/* Enlaces a redes sociales */}
+                            <a href="https://twitter.com/bienaldelchaco" target="_blank" rel="noopener noreferrer"
+                               className="relative">
+                                <i className="fab fa-twitter text-2xl hover:text-white transition-colors"></i>
+                            </a>
+                            <a href="https://www.youtube.com/BienaldelChaco" target="_blank" rel="noopener noreferrer"
+                               className="relative">
+                                <i className="fab fa-youtube text-2xl hover:text-white transition-colors"></i>
+                            </a>
+                            <a href="https://www.facebook.com/bienaldelchaco" target="_blank" rel="noopener noreferrer"
+                               className="relative">
+                                <i className="fab fa-facebook-f text-2xl hover:text-white transition-colors"></i>
+                            </a>
+                            <a href="https://www.instagram.com/bienaldelchaco/" target="_blank"
+                               rel="noopener noreferrer"
+                               className="relative">
+                                <i className="fab fa-instagram text-2xl hover:text-white transition-colors"></i>
+                            </a>
+                        </div>
+                    </div>
 
-            {/* Footer se encuentra al final de la sección */}
-            <Footer/>
+                    {/* Logo */}
+                    <div className="flex justify-start items-center">
+                        <img src={logo} alt="Logo"
+                             className="h-16 sm:h-[116px] object-contain"/> {/* Logo más pequeño en móvil */}
+                    </div>
+                </div>
+
+                {/* Derechos de autor */}
+                <div className="text-center mt-6">
+                    <p className="text-sm opacity-80">
+                        Copyright © {new Date().getFullYear()} - All rights reserved by Bienal Internacional de
+                        Escultura
+                        del Chaco
+                    </p>
+                </div>
+            </footer>
         </section>
     );
 };
